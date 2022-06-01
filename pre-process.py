ab81309c6bde885db1f1d8aa507e1a704ecb5aad
Treavor Gagne <gagne.16@hotmail.com>
2022-05-31 21:53:40 -0700

    Update to filter courses specific to stream seq

--- pre-process.py
+++ pre-process.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import re
 
 """
 
@@ -71,35 +72,92 @@ def main():
     print("[")
     for entry in data:
         if entry != None:
+            
             if entry['scheduleTypeDescription'] != "Lab" and entry['scheduleTypeDescription'] != "Tutorial":
-                if entry['subject'] == "CSC":
-                    if entry['courseNumber'] in ['111', '115', '230', "225", '361', '320', '360', '370', '226', '460']:
-                        print(json.dumps(del_fields(entry),indent=2))
-                        print(",")
-                elif entry['subject'] == "SENG":
-                    if entry['courseNumber'] in ["265", "275", "310", "321", "371", "350", "360", "426", "440", "499", "401"]:
-                        print(json.dumps(del_fields(entry),indent=2))
-                        print(",")
-                elif entry['subject'] == "ECE":
-                    if entry['courseNumber'] in ["255", "260", "310", "458", "355", "360", "360", "455"]:
-                        print(json.dumps(del_fields(entry),indent=2))
-                        print(",")
-                elif entry['subject'] == "MATH":
-                    if entry['courseNumber'] in ["100", "109", "110", "101", "122", "360"]:
-                        print(json.dumps(del_fields(entry),indent=2))
-                        print(",")
-                elif entry['subject'] == "ENGR":
-                    if entry['courseNumber'] in ["120", "110", "130", "141"]:
-                        print(json.dumps(del_fields(entry),indent=2))
-                        print(",")
-                elif entry['subject'] == "CHEM":
-                    if entry['courseNumber'] in ["101", "150"]:
-                        print(json.dumps(del_fields(entry),indent=2))
-                        print(",")
-                elif entry['subject'] == "PHYS":
-                    if entry['courseNumber'] in ["110", "111"]:
-                        print(json.dumps(del_fields(entry),indent=2))
-                        print(",")
+                # fall courses 
+                if re.search("^.*09$", entry["term"]): 
+                    if entry['subject'] == "CSC":
+                        if entry['courseNumber'] in ['111', '115', '225', '226', '230', '320', '355', '360', '361', '370', '355']:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "SENG":
+                        if entry['courseNumber'] in ["265", '310', '321', "350", "360", '421', '474']:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "ECE":
+                        if entry['courseNumber'] in ["255", "260", "355"]:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "MATH":
+                        if entry['courseNumber'] in ["100", "109", "110", "122"]:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "ENGR":
+                        if entry['courseNumber'] in ["110", "130"]:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "CHEM":
+                        if entry['courseNumber'] == "101":
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "PHYS":
+                        if entry['courseNumber'] == "110":
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "STAT":
+                        if entry['courseNumber'] == "260":
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                
+                # spring courses
+                elif re.search("^.*01$", entry["term"]): 
+                    if entry['subject'] == "CSC":
+                        if entry['courseNumber'] in ['111', '115', '225', '226', '230', "320", "360", "370", '361', '460']:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "SENG":
+                        if entry['courseNumber'] in ["265", "275", "310", "321", "371", "401", "411", "468", "474"]:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "ECE":
+                        if entry['courseNumber'] in ["458", "360", "455"]:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "MATH":
+                        if entry['courseNumber'] in ["101"]:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "ENGR":
+                        if entry['courseNumber'] in ["120", "141"]:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "CHEM":
+                        if entry['courseNumber'] == "150":
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "PHYS":
+                        if entry['courseNumber'] == "111":
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                
+                # summer courses
+                elif re.search("^.*05$", entry["term"]): 
+                    if entry['subject'] == "CSC":
+                        if entry['courseNumber'] in ['115', '225', '226', '230', "320", "360", "370"]:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "SENG":
+                        if entry['courseNumber'] in ["265", "275", "310", "426", "440", "499", "275", "310"]:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "ECE":
+                        if entry['courseNumber'] in ["310"]:
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
+                    elif entry['subject'] == "ECON":
+                        if entry['courseNumber'] == "180":
+                            print(json.dumps(del_fields(entry),indent=2))
+                            print(",")
 
     print("\t null\n]")
 
