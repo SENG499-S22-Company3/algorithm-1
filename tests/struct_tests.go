6df3989d12d7c19ec049bd54509373e615edc152
BrodyGimson <brody.gimson@gmail.com>
2022-06-01 13:17:55 -0700

    Added test file for structs

---
+++ tests/struct_tests.go
@@ -0,0 +1,12 @@
+package tests
+
+import (
+	"algorithm-1/structs"
+	"testing"
+)
+
+func TestAssignmentStruct(t *testing.T) {
+	testCourse := structs.Course{}
+	testProfessor := structs.Professor{}
+	testAssignment := structs.Assignment{}
+}
