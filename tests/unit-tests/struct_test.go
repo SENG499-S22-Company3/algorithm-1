9ad7b1af02ae27d30f9e700e98d8e8f7b6e0de5e
BrodyGimson <brody.gimson@gmail.com>
2022-06-02 10:38:15 -0700

    Added some basic struct tests

--- tests/unit-tests/struct_test.go
+++ tests/unit-tests/struct_test.go
@@ -35,7 +35,32 @@ func TestCourseStruct(t *testing.T) {
 }
 
 func TestProfessorStuct(t *testing.T) {
+	testCourse := structs.Course{
+		CourseNumber:      courseNum,
+		Subject:           courseSub,
+		SequenceNumber:    courseSeq,
+		CourseTitle:       courseTitle,
+		RequiredEquipment: courseEquipment,
+		RequiresPEng:      requiresPEng,
+	}
 
+	testPreference := structs.Preference{
+		Course:        testCourse,
+		PreferenceNum: 195,
+	}
+
+	testProfessor := structs.Professor{
+		Preferences:       []structs.Preference{testPreference},
+		CoursesCanTeach:   []structs.Course{testCourse},
+		DisplayName:       displayName,
+		TeachingStatus:    teachingStatus,
+		RequiredEquipment: profEquipment,
+		HasPEng:           hasPEng,
+	}
+
+	if testProfessor.Preferences[0].Course.CourseNumber != 370 {
+		t.Errorf("Got %v, expected %v", testProfessor.Preferences[0].Course.CourseNumber, 370)
+	}
 }
 
 func TestAssignmentStruct(t *testing.T) {
