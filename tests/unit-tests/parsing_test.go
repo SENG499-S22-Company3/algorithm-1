1ca7672f01696c7666f8ebf9dd7f10047dd55122
Brody Gimson <66978819+BrodyGimson@users.noreply.github.com>
2022-06-07 13:47:10 -0700

    Parsing JSON Historical Data (#68)
    * Updated structs and made parsing method with tests
    
    * Removed print statement in test
    
    * Added hoursWeek field and updated tests
    
    * Updated parsing test
    
    * Updated parsing tests

--- tests/unit-tests/parsing_test.go
+++ tests/unit-tests/parsing_test.go
@@ -1,7 +1,62 @@
 package tests
 
-import "testing"
+import (
+	"algorithm-1/structs"
+	"testing"
+)
 
-func TestCourseParse(t *testing.T) {
+func TestSmallHistoricalParse(t *testing.T) {
+	jsonData := []byte(`
+		{
+			"fallTermCourses": [
+				{
+				"courseNumber": 101,
+				"subject": "CHEM",
+				"sequenceNumber": "A01",
+				"courseTitle": "Properties of Materials",
+				"meetingTime": {
+					"beginTime": 1300,
+					"endDate": "Dec 05, 2018",
+					"endTime": 1420,
+					"friday": false,
+					"hoursWeek": 3,
+					"monday": true,
+					"saturday": false,
+					"startDate": "Sep 05, 2018",
+					"sunday": false,
+					"thursday": true,
+					"tuesday": false,
+					"wednesday": false
+				}
+				}
+			],
+			"springTermCourses": [],
+			"summerTermCourses": []
+		}`)
 
+	testSchedule := structs.ParseHistorical(jsonData)
+
+	if testSchedule.FallCourses == nil {
+		t.Error("Schedule failed to be parsed")
+	} else if testSchedule.FallCourses[0].CourseNumber != 101 && !testSchedule.FallCourses[0].Assignment.Thursday {
+		t.Errorf("Schedule successfully parsed, but data is incorrect. Course number should be 101 and it was %v, and/or Thursday should be true when it was %v",
+			testSchedule.FallCourses[0].CourseNumber, testSchedule.FallCourses[0].Assignment.Thursday)
+	}
+}
+
+func TestLargeHistoricalParse(t *testing.T) {
+	var testSchedule structs.Schedule
+
+	jsonData := []byte(`
+		{
+			"fallTermCourses": [],
+			"springTermCourses": [],
+			"summerTermCourses": []
+		}`)
+
+	testSchedule = structs.ParseHistorical(jsonData)
+
+	if testSchedule.FallCourses == nil {
+		t.Error("Schedule failed to be parsed")
+	}
 }
