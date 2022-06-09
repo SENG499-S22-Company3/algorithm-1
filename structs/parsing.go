1f7c5a0fc4f794577f12020917764e0515bf063f
Treavor Gagne <gagne.16@hotmail.com>
2022-06-09 01:15:06 -0700

    go parsing to get historical to 2018-19 schedule

--- structs/parsing.go
+++ structs/parsing.go
@@ -2,6 +2,7 @@ package structs
 
 import (
 	"encoding/json"
+	"regexp"
 )
 
 func ParseHistorical(jsonData []byte) Schedule {
@@ -14,6 +15,31 @@ func ParseHistorical(jsonData []byte) Schedule {
 	}
 
 	return parsedSchedule
+}
+
+func Schedule2019(schedule Schedule) Schedule {
+	var schedule2019 Schedule
+	
+	for _, x := range schedule.FallCourses {
+		match, _ := regexp.MatchString("Sep .., 2018", x.Assignment.StartDate)
+		if match {
+			schedule2019.FallCourses = append(schedule2019.FallCourses, x)
+		}
+	}
+	for _, x := range schedule.SpringCourses {
+		match, _ := regexp.MatchString("Jan .., 2019", x.Assignment.StartDate)
+		if match {
+			schedule2019.SpringCourses = append(schedule2019.SpringCourses, x)
+		}
+	}
+	for _, x := range schedule.SummerCourses {
+		match, _ := regexp.MatchString("May .., 2019", x.Assignment.StartDate)
+		if match {
+			schedule2019.SummerCourses = append(schedule2019.SummerCourses, x)
+		}
+	}
+
+	return schedule2019
 }
 
 func ParseCourses(jsonData []byte) []Course {
