d74880ea4929d297dc5cf7fea81dee6417f6d711
Treavor Gagne <gagne.16@hotmail.com>
2022-07-05 14:09:58 -0700

    splitting courses with test

---
+++ tests/unit-tests/course_split_test.go
@@ -0,0 +1,56 @@
+package tests
+
+import (
+	"algorithm-1/scheduling"
+	"algorithm-1/structs"
+	"testing"
+)
+
+func TestSplitCourses(t *testing.T) {
+	// Setup
+	jsonData := []byte(
+		`[{
+          "courseNumber": "265",
+          "subject": "SENG",
+          "sequenceNumber": "A01",
+          "courseTitle": "Intro to Software Development",
+          "StreamSequence": "2B",
+          "numSections": 1,
+          "courseCapacity": 477
+        },
+		{
+			"courseNumber": "226",
+			"subject": "SENG",
+			"sequenceNumber": "A01",
+			"courseTitle": "Algorithms and Data Structures II",
+			"StreamSequence": "3A",
+			"numSections": 1,
+			"courseCapacity": 225
+		  },
+        {
+          "courseNumber": "225",
+          "subject": "CSC",
+          "sequenceNumber": "A01",
+          "courseTitle": "Algorithms and Data Structures I",
+          "StreamSequence": "2B",
+          "numSections": 2,
+          "courseCapacity": 345
+        },
+        {
+          "courseNumber": "111",
+          "subject": "CSC",
+          "sequenceNumber": "A01",
+          "courseTitle": "Fundamentals of Programming with Engineering Applications",
+          "StreamSequence": "1A",
+          "numSections": 2,
+          "courseCapacity": 436
+        }]`)
+
+	courses, err := structs.ParseCourses(jsonData)
+	courses = scheduling.Split(courses)
+
+	if err != nil {
+		t.Error("Parsing courses failed with error: ", err.Error())
+	}
+
+}
