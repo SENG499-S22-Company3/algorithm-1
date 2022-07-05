64e5d7b99683cef8cd604e0888f2750064673640
brodygimson <brodygimson@uvic.ca>
2022-07-05 16:35:35 -0700

    Added timeslot failure test

--- tests/unit-tests/server_test.go
+++ tests/unit-tests/server_test.go
@@ -50,6 +50,46 @@ func TestCheckSchdulePass(t *testing.T) {
 	}
 }
 
-func TestCheckScheduleFail(t *testing.T) {
-	// TO-DO add failure tests
+func TestCheckScheduleProfFail(t *testing.T) {
+	// Setup
+	jsonFile, err := os.Open("../data/prof-violation-schedule-test.json")
+	if err != nil {
+		t.Error("File not found")
+	}
+
+	request, _ := http.NewRequest(http.MethodPost, "/CheckSchedule", jsonFile)
+	response := httptest.NewRecorder()
+
+	// Act
+	server.CheckSchedule(response, request)
+
+	// Assert
+	actual_response := response.Body.String()
+	expected_response := "error: Bird, Bill teaching another Fall course at MTh1300,   Schedule given has some violations that should be resolved"
+
+	if actual_response != expected_response {
+		t.Errorf("got %q, want %q", actual_response, expected_response)
+	}
+}
+
+func TestCheckScheduleTimeslotFail(t *testing.T) {
+	// Setup
+	jsonFile, err := os.Open("../data/timeslot-violation-schedule-test.json")
+	if err != nil {
+		t.Error("File not found")
+	}
+
+	request, _ := http.NewRequest(http.MethodPost, "/CheckSchedule", jsonFile)
+	response := httptest.NewRecorder()
+
+	// Act
+	server.CheckSchedule(response, request)
+
+	// Assert
+	actual_response := response.Body.String()
+	expected_response := "error: SENG 275 is scheduled at same time as another required course SENG310,   Schedule given has some violations that should be resolved"
+
+	if actual_response != expected_response {
+		t.Errorf("got %q, want %q", actual_response, expected_response)
+	}
 }
